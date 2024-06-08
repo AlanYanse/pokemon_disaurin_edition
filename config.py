ca6bcf76ad80048625951ad069899a47cad90685
AlanYanse <yansealan@gmail.com>
2024-06-07 22:40:37 -0300

    estructura base del juego

---
+++ config.py
@@ -0,0 +1,18 @@
+
+# Constantes de la ventana del juego
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+
+
+# Constante de la capa del jugador
+PLAYER_LAYER = 1
+
+#
+TILE_SIZE = 32
+
+#
+RED = (255,0,0)
+BLACK = (0,0,0)
+
+#
+FPS = 60
