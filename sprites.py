21f6f0e93d943f2680b94371127f013fcdb84e83
AlanYanse <yansealan@gmail.com>
2024-06-08 22:02:33 -0300

    mejorando los sprites

--- sprites.py
+++ sprites.py
@@ -12,6 +12,8 @@ class Spritesheet:
     
     def get_sprite(self, x, y, width, height):
         sprite = pygame.Surface([width, height])
+        sprite = sprite.convert_alpha()
+        sprite.fill((0, 0, 0, 0))
         sprite.blit(self.sheet, (0,0), (x,y,width,height))
         sprite.set_colorkey(BLACK)
         return sprite
