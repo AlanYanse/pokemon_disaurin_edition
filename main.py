34e43135674683cd79b9f58b31d40e83f81c5a0e
Especial2x4 <especial2x4@gmail.com>
2024-06-13 23:07:47 -0300

    implementando menú de game over

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Game:
         self.enemy_spritesheet = Spritesheet("src/img/enemy.png")
 
         self.intro_background = pygame.image.load("src/img/introbackground.png")
+        self.go_background = pygame.image.load("src/img/gameover.png")
         
         
 
@@ -78,12 +79,44 @@ class Game:
             self.update()
             self.draw()
 
-        self.running = False
+        #self.running = False
 
 
     
     def game_over(self):
-        pass
+        text = self.font.render("Game Over", True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = Button(10, WIN_HEIGHT - 60, 120, 50, WHITE, BLACK, "Restart", 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
+
+
+
+
+
+
+
     
 
     def intro_screen(self):
